0337f572add4ba832c8aeff47bc77e01cf2f15ea
Paul Steffen <p.steffen@betclicgroup.com>
2023-06-13 14:18:26 +0200

    Add resizable model on pr version which fix install issues

--- lightfm/__init__.py
+++ lightfm/__init__.py
@@ -1,4 +1,4 @@
-from .lightfm import LightFM
+from .lightfm import LightFM, LightFMResizable
 from .version import __version__
 
-__all__ = ["LightFM", "datasets", "evaluation", "__version__"]
+__all__ = ["LightFM", "LightFMResizable", "datasets", "evaluation", "__version__"]
